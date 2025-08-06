ee9e600c0c02a330030c97c1e29fb7634c7e6c95
Hamiat <hamiat.n@hotmail.com>
2025-08-06 15:13:44 +0200

    Refactored Code

--- main.py
+++ main.py
@@ -2,41 +2,51 @@ import requests
 from azure.cosmos import CosmosClient, exceptions
 from config import config
 
-con = config()
-print(con)
+def Fetch_Products():
 
-# Cosmos DB client
-client = CosmosClient(con["endpoint"], con["key"])
-db = client.get_database_client(con["database_name"])
-container = db.get_container_client(con["container_name"])
+    con = config()
 
-# Fetch products
-for i in range(1, 100):
-    url = f"https://dummyjson.com/products/{i}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        product = response.json()
-        product["id"] = str(product["id"])
-        container.upsert_item(product)
-    else:
-        print(f"Failed to fetch product. Status: {response.status_code}")
+    # Cosmos DB client
+    client = CosmosClient(con["endpoint"], con["key"])
+    db = client.get_database_client(con["database_name"])
+    container = db.get_container_client(con["container_name"])
 
-print("******************")
+    # Fetch products
+    for i in range(1, 100):
+        url = f"https://dummyjson.com/products/{i}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            product = response.json()
+            product["id"] = str(product["id"])
+            container.upsert_item(product)
+        else:
+            print(f"Failed to fetch product. Status: {response.status_code}")
+    
+    return container
 
-# Query average price
-query = "SELECT VALUE AVG(c.price) FROM c"
-items = list(container.query_items(query=query, enable_cross_partition_query=True))
-avg_price = items[0] 
-print(f"Average price: {avg_price}")
 
-print("******************")
-# Add a product to my database
-product = {
-    "id" : "1001",
-    "name" : "Wow Cool product",
-    "category" : "Books",
-    "price" : 39.99
-}
+def Queries(container):
 
-#container.create_item(body=product)
-print("Created item: ", product)
+    # Query average price
+    query = "SELECT VALUE AVG(c.price) FROM c"
+    items = list(container.query_items(query=query, enable_cross_partition_query=True))
+    avg_price = items[0] 
+    print(f"Average price: {avg_price}")
+
+    # Add a product to my database
+    product = {
+        "id" : "1001",
+        "name" : "Rick and Morty: Vol 1",
+        "category" : "Books",
+        "price" : 39.99
+    }
+
+    #container.create_item(body=product)
+
+def Main():
+    products_container = Fetch_Products()
+    Queries(products_container)
+
+
+if __name__ == "__main__":
+    Main()
